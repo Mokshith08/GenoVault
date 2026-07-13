50c5448ff30aae9868de32dc0dc00a90424b8176
Mokshith <mokshithmendon978@gmail.com>
2026-07-13 21:03:58 +0530

    Added audit

---
+++ backend/routes/auditRoutes.js
@@ -0,0 +1,6 @@
+﻿const express = require('express');
+const router  = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const { getAudit } = require('../controllers/auditController');
+router.get('/', protect, getAudit);
+module.exports = router;
